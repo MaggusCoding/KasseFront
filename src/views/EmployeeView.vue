8947d4487cc615c1c8a8b1336e97a31df41e8abb
MerveKesici <kesici_m@yahoo.de>
2023-06-19 16:02:55 +0200

    env

--- src/views/EmployeeView.vue
+++ src/views/EmployeeView.vue
@@ -5,11 +5,10 @@
     <div class="row row-cols-1 row row-cols-md-4 g-4">
       <div class="col" v-for="person in persons" :key="person.id">
         <div class="card h-100">
-          <img :src="getAvatar(person)" class="card-img-top" :alt="person.firstname + ' ' + person.lastname">
           <div class="card-body">
-            <h5 class="card-title">{{ person.firstname }} {{ person.lastname }}</h5>
+            <h5 class="card-title">{{ person.vorname }} {{ person.nachname }}</h5>
             <p class="card-text">
-              {{ person.firstname }} {{ person.lastname }}
+              {{ person.vorname }} {{ person.nachname }}
             </p>
           </div>
         </div>
@@ -23,30 +22,21 @@ export default {
   name: 'EmployeeView',
   data () {
     return {
-      persons: [
-        {
-          id: 1,
-          firstname: 'Maggus',
-          lastname: 'Meeresdublone',
-          gender: 'MALE'
-        },
-        {
-          id: 2,
-          firstname: 'Merv',
-          lastname: 'van Nerv',
-          gender: 'FEMALE'
-        }
-      ]
+      persons: []
     }
   },
-  methods: {
-    getAvatar (person) {
-      if (person.gender === 'MALE') {
-        return require('../assets/Mann.png')
-      } else if (person.gender === 'FEMALE') {
-        return require('../assets/Frau.png')
-      }
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + 'api/mitarbeiter'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
     }
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(person => {
+        this.persons.push(person)
+      }))
+      .catch(error => console.log('error', error))
   }
 }
 </script>
