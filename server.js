60fcd7a4f3ea0ae3faa8e5364b0ac15bd1496bca
MaggusCoding <mseethaler98@gmail.com>
2023-07-09 20:08:20 +0200

    fixed router issue

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const history = require('connect-history-api-fallback')
-app.use(express.static('dist')).use(history())
+const historyApiFallback = require('connect-history-api-fallback');
+app.use(express.static('dist')).use(history(historyApiFallback))
 
 app.listen(3000, () => {
   console.log('Server listening on: http://localhost:%s', 3000)
