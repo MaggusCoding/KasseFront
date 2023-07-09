51d652780997f4112ab7b5e0a8552b6259f464ad
MaggusCoding <mseethaler98@gmail.com>
2023-07-09 20:08:26 +0200

    fixed router issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const history = require('connect-history-api-fallback')
-const historyApiFallback = require('connect-history-api-fallback');
+const historyApiFallback = require('connect-history-api-fallback')
 app.use(express.static('dist')).use(history(historyApiFallback))
 
 app.listen(3000, () => {
